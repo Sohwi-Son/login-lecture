bd9b9470cc2b2e70d681f6e2b2bf2bc9bab54504
SoHee <ksl428302@gmail.com>
2022-09-21 16:40:29 +0900

    fs모듈을 총한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,42 +1,47 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
-    static #users = {
-        id: ["손소휘" , "서필립"],
-        psword: ["1234" , "1234"],
-        name: ["공쥬님" , "서아서필립"],
-      };
+  static #getUserInfo() {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // =. [id, psword, name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
 
-      static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers,field) => {
-          if (users.hasOwnProperty(field)) {
-            newUsers[field] = users[field]
-          }
-          return newUsers;
-         }, {});
-        return newUsers;
-      }
+    return userInfo;
+  }
+  
+  static getUsers(...fields) {
+  // const users = this.#users;
+  const newUsers = fields.reduce((newUsers,field) => {
+    if (users.hasOwnProperty(field)) {
+      newUsers[field] = users[field]
+    }
+    return newUsers;
+    }, {});
+  return newUsers;
+}
 
-      static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf();
-        const usersKeys = Object.keys(users); // =. [id, psword, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-          newUser[info] = users[info][idx];
-          return newUser;
-        }, {});
+  static getUserInfo(id) {
+    return fs
+      .readFile("./srcldatabases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
+  }
 
-        return userInfo;
-      }
-
-      static save(userInfo) {
-        const users = this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.paword.push(userInfo.paword);
-        return {success: true};
-      }
+  static save(userInfo) {
+    // const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.paword.push(userInfo.paword);
+    return {success: true};
+  }
 }
 
 module.exports = UserStorage;
